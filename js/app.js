7999d65db712152dc51b8bc50d028781a833b4a5
akashwebs <aksah.shil227@gmail.com>
2022-02-28 18:23:05 +0600

    fetch api and result show

--- js/app.js
+++ js/app.js
@@ -10,4 +10,35 @@ serachButton.addEventListener('click', async function() {
         const searchValue = parseInt(serachInput.value)
         displayResult(data.splice(0, searchValue));
     }
+    serachInput.value = ''
 })
+const studentDetailsContainer = document.getElementById('student-details-container');
+const displayResult = data => {
+    studentDetailsContainer.innerHTML = ''
+    if (data.length == 0) {
+        console.log('no data found')
+    } else {
+        data.forEach(res => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            const title = res.title;
+            const img = res.thumbnailUrl;
+
+            div.innerHTML = `
+                <div class="card">
+                    <img src="${res.thumbnailUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <button onclick="seeDetails(${res.id})" class="btn btn-success">Details</button>
+                        <button class="btn btn-danger">Remove</button>
+                    </div>
+                </div>
+            `
+            studentDetailsContainer.appendChild(div)
+        })
+
+
+    }
+}
+const seeDetails = (id) => {
+    console.log(id);
+}
